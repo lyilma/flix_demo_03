32c63fddce9aa7a1cf3dc7c35b48a2452de8455e
user142255 <user142255@as121.local>
2018-09-25 09:47:03 +0800

    Initial Commit

---
+++ flix_demo_03/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix_demo_03
+//
+//  Created by user142255 on 9/25/18.
+//  Copyright © 2018 user142255. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ flix_demo_03Tests/flix_demo_03Tests.swift
@@ -0,0 +1,34 @@
+//
+//  flix_demo_03Tests.swift
+//  flix_demo_03Tests
+//
+//  Created by user142255 on 9/25/18.
+//  Copyright © 2018 user142255. All rights reserved.
+//
+
+import XCTest
+@testable import flix_demo_03
+
+class flix_demo_03Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
